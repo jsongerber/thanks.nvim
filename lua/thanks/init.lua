c6b1299b9af70c29a1733ebf81943d37875e989c
Jason Gerber <jason@jasongerber.ch>
2024-04-09 23:31:41 +0200

    unstar wip

--- lua/thanks/init.lua
+++ lua/thanks/init.lua
@@ -2,8 +2,9 @@ local M = {}
 
 M.default_config = {
 	plugin_manager = "",
-	star_on_startup = true,
+	star_on_startup = false,
 	star_on_install = true,
+	unstar_on_uninstall = false,
 	ignore_repos = {},
 	ignore_authors = {},
 }
@@ -13,18 +14,29 @@ M.get_config = function(options)
 	return vim.tbl_deep_extend("force", M.default_config, options or {})
 end
 
+-- Stars or unstars a plugin
+-- To go easy on the Github API rate limit, we star/unstar plugins in intervals
+-- This function is called recursively until all plugins are starred/unstarred
+-- Having real async functions would help for testing, but this is good enough for now
+-- Will be refactored when I'm less dumb
 ---@param github GithubApi
----@param plugins Plugin[]
+---@param to_star Plugin[]
+---@param to_unstar string[]
+---@param data { starred_plugins: string[] }
 ---@param index number
----@param stats { starred: number, ignored: number, already_starred: number }
+---@param stats { starred: number, unstarred: number, ignored: number, already_starred: number }
 ---@param called_from_command boolean
-local function star_interval(github, plugins, index, stats, called_from_command)
-	if index > #plugins then
+local function star_interval(github, to_star, to_unstar, data, index, stats, called_from_command)
+	if index > #to_star + #to_unstar then
 		if stats.starred > 0 or called_from_command then
 			local message = "Starred " .. stats.starred .. " plugins"
 
 			if stats.already_starred > 0 then
 				message = message .. " - " .. stats.already_starred .. " already starred"
+			end
+
+			if stats.unstarred > 0 then
+				message = message .. " - " .. stats.unstarred .. " unstarred"
 			end
 
 			if stats.ignored > 0 then
@@ -36,41 +48,67 @@ local function star_interval(github, plugins, index, stats, called_from_command)
 		return
 	end
 
+	local plugin_handle = nil
+	local plugin_author = nil
+	if index <= #to_star then
+		-- Star
+		plugin_handle = to_star[index].handle
+		plugin_author = to_star[index].author
+	else
+		-- Unstar
+		plugin_handle = to_unstar[index - #to_star]
+		local parts = vim.split(plugin_handle, "/")
+		plugin_author = parts[1]
+	end
+
+	if not plugin_handle then
+		star_interval(github, to_star, to_unstar, data, index + 1, stats, called_from_command)
+		return
+	end
+
 	-- Check if plugin is ignored
-	if vim.tbl_contains(M.config.ignore_repos, plugins[index].handle) then
+	if vim.tbl_contains(M.config.ignore_repos, plugin_handle) then
 		stats.ignored = stats.ignored + 1
-		star_interval(github, plugins, index + 1, stats, called_from_command)
+		star_interval(github, to_star, to_unstar, data, index + 1, stats, called_from_command)
 		return
 	end
 
 	-- Check if author is ignored
-	if vim.tbl_contains(M.config.ignore_authors, plugins[index].author) then
+	if vim.tbl_contains(M.config.ignore_authors, plugin_author) then
 		stats.ignored = stats.ignored + 1
-		star_interval(github, plugins, index + 1, stats, called_from_command)
+		star_interval(github, to_star, to_unstar, data, index + 1, stats, called_from_command)
 		return
 	end
 
-	-- Check if plugin is already starred
-	local data = require("thanks.utils").read_persisted_data()
 	local starred_plugins = data.starred_plugins or {}
-	if vim.tbl_contains(starred_plugins, plugins[index].handle) then
-		stats.already_starred = stats.already_starred + 1
-		star_interval(github, plugins, index + 1, stats, called_from_command)
-		return
+	if index <= #to_star then
+		stats.starred = stats.starred + 1
+		github:star(plugin_handle)
+
+		-- Save the starred plugin to disk
+		table.insert(starred_plugins, plugin_handle)
+		data.starred_plugins = starred_plugins
+	else
+		stats.unstarred = stats.unstarred + 1
+		github:star(plugin_handle, false)
+
+		-- Remove the unstarred plugin from disk
+		for i, starred_plugin in ipairs(starred_plugins) do
+			if starred_plugin == plugin_handle then
+				table.remove(starred_plugins, i)
+				break
+			end
+		end
 	end
-
-	stats.starred = stats.starred + 1
-	github:star(plugins[index])
-
-	-- Save the starred plugin
-	table.insert(starred_plugins, plugins[index].handle)
-	data.starred_plugins = starred_plugins
 	require("thanks.utils").persist_data(data)
 
 	vim.defer_fn(function()
-		vim.notify("Successfully starred " .. plugins[index].name, vim.log.levels.INFO)
+		vim.notify(
+			"Successfully " .. (index > #to_star and "unstarred " or "starred ") .. plugin_handle,
+			vim.log.levels.INFO
+		)
 
-		star_interval(github, plugins, index + 1, stats, called_from_command)
+		star_interval(github, to_star, to_unstar, data, index + 1, stats, called_from_command)
 	end, 500)
 end
 
@@ -130,7 +168,9 @@ M.setup = function(options)
 			}),
 			pattern = event,
 			callback = function()
-				M.star_all(false)
+				vim.schedule(function()
+					M.star_all(false)
+				end)
 			end,
 		})
 	end
@@ -138,16 +178,25 @@ end
 
 ---@param called_from_command boolean
 M.star_all = function(called_from_command)
-	local plugins = require("thanks.utils").get_plugins(M.config.plugin_manager)
+	local utils = require("thanks.utils")
+	local installed_plugins = utils.get_plugins(M.config.plugin_manager)
+	local data = utils.read_persisted_data() or {}
+	local cached_plugins = data.starred_plugins or {}
+
+	local to_unstar = M.config.unstar_on_uninstall and utils.get_plugins_to_unstar(installed_plugins, cached_plugins)
+		or {}
+	local to_star = utils.get_plugins_to_star(installed_plugins, cached_plugins)
+
 	local github = require("thanks.github_api"):new()
 
 	local stats = {
+		unstarred = 0,
 		starred = 0,
 		ignored = 0,
 		already_starred = 0,
 	}
 	if github:get_auth() then
-		star_interval(github, plugins, 1, stats, called_from_command)
+		star_interval(github, to_star, to_unstar, data, 1, stats, called_from_command)
 	else
 		vim.notify("Please authenticate with Github first using :ThanksGithubAuth", vim.log.levels.INFO)
 	end
--- lua/thanks/tests/utils.lua
+++ lua/thanks/tests/utils.lua
@@ -1,7 +1,7 @@
 local utils = require("thanks.utils")
 local testfile = "jsongerber-thanks-test.json"
 
-describe("utils", function()
+describe("read_and_write_data_to_disk", function()
 	after_each(function()
 		vim.fn.delete(vim.fn.stdpath("data") .. "/" .. testfile)
 	end)
@@ -9,7 +9,7 @@ describe("utils", function()
 	it("should return an empty table if the dir does not exist", function()
 		vim.fn.delete(vim.fn.stdpath("data") .. "/" .. testfile)
 
-		assert.are.same({}, utils.read_persisted_data(testfile))
+		assert.same({}, utils.read_persisted_data(testfile))
 	end)
 
 	it("should persist and read a data", function()
@@ -21,5 +21,133 @@ describe("utils", function()
 		utils.persist_data(data, testfile)
 
 		assert.same(data, utils.read_persisted_data(testfile))
+	end)
+end)
+
+describe("get_plugins_to_star", function()
+	it("should return plugins that are not starred", function()
+		local plugins = {
+			{ handle = "a" },
+			{ handle = "b" },
+			{ handle = "c" },
+		}
+		local cached_plugins = {
+			"a",
+		}
+
+		assert.same({ plugins[2], plugins[3] }, utils.get_plugins_to_star(plugins, cached_plugins))
+
+		plugins = {
+			{ handle = "a" },
+			{ handle = "b" },
+			{ handle = "c" },
+		}
+
+		cached_plugins = {
+			"a",
+			"b",
+			"c",
+		}
+
+		assert.same({}, utils.get_plugins_to_star(plugins, cached_plugins))
+
+		plugins = {
+			{ handle = "a" },
+			{ handle = "b" },
+			{ handle = "c" },
+		}
+
+		cached_plugins = {
+			"a",
+			"b",
+		}
+
+		assert.same({ plugins[3] }, utils.get_plugins_to_star(plugins, cached_plugins))
+
+		plugins = {
+			{ handle = "a" },
+			{ handle = "b" },
+			{ handle = "c" },
+		}
+
+		cached_plugins = {}
+
+		assert.same(plugins, utils.get_plugins_to_star(plugins, cached_plugins))
+
+		plugins = {
+			{ handle = "a" },
+			{ handle = "b" },
+			{ handle = "c" },
+		}
+
+		cached_plugins = {
+			"a",
+			"b",
+			"c",
+			"d",
+		}
+
+		assert.same({}, utils.get_plugins_to_star(plugins, cached_plugins))
+	end)
+
+	it("should return plugins that are starred but not installed", function()
+		local plugins = {
+			{ handle = "a" },
+			{ handle = "b" },
+		}
+
+		local cached_plugins = {
+			"a",
+			"b",
+			"c",
+		}
+
+		assert.same({ cached_plugins[3] }, utils.get_plugins_to_unstar(plugins, cached_plugins))
+
+		plugins = {
+			{ handle = "a" },
+			{ handle = "b" },
+		}
+
+		cached_plugins = {
+			"a",
+			"b",
+		}
+
+		assert.same({}, utils.get_plugins_to_unstar(plugins, cached_plugins))
+
+		plugins = {
+			{ handle = "a" },
+			{ handle = "b" },
+		}
+
+		cached_plugins = {
+			"a",
+		}
+
+		assert.same({}, utils.get_plugins_to_unstar(plugins, cached_plugins))
+
+		plugins = {
+			{ handle = "a" },
+			{ handle = "b" },
+		}
+
+		cached_plugins = {}
+
+		assert.same({}, utils.get_plugins_to_unstar(plugins, cached_plugins))
+
+		plugins = {
+			{ handle = "a" },
+			{ handle = "b" },
+		}
+
+		cached_plugins = {
+			"a",
+			"b",
+			"c",
+			"d",
+		}
+
+		assert.same({ cached_plugins[3], cached_plugins[4] }, utils.get_plugins_to_unstar(plugins, cached_plugins))
 	end)
 end)
